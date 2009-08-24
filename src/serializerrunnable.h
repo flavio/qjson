333624494a3d6da53ea379a8b8c3865e917c4d21
micron <micron@283d02a7-25f6-0310-bc7c-ecb5cbfe19da>
2009-08-24 15:04:56 +0000

    moved the SerializerRunnable class inside QJson namespace
    git-svn-id: https://svn.kde.org/home/kde/trunk/playground/libs/qjson@1015115 283d02a7-25f6-0310-bc7c-ecb5cbfe19da

--- src/serializerrunnable.h
+++ src/serializerrunnable.h
@@ -30,43 +30,45 @@ class QByteArray;
 class QString;
 class QVariant;
 
-/**
-* @brief Convenience class for converting JSON data to QVariant objects using a dedicated thread
-*/
-class QJSON_EXPORT SerializerRunnable  : public QObject, public QRunnable
-{
-  Q_OBJECT
-  public:
-    /**
-    * This signal is emitted when the conversion process has been completed
-    * @param data contains the JSON data that has to be converted
-    * @param parent parent of the object
-    **/
-    explicit SerializerRunnable(QObject* parent = 0);
-    ~SerializerRunnable();
+namespace QJson {
+  /**
+  * @brief Convenience class for converting JSON data to QVariant objects using a dedicated thread
+  */
+  class QJSON_EXPORT SerializerRunnable  : public QObject, public QRunnable
+  {
+    Q_OBJECT
+    public:
+      /**
+      * This signal is emitted when the conversion process has been completed
+      * @param data contains the JSON data that has to be converted
+      * @param parent parent of the object
+      **/
+      explicit SerializerRunnable(QObject* parent = 0);
+      ~SerializerRunnable();
 
-    /**
-     * Sets the json object to serialize.
-     * 
-     * @param json QVariant containing the json representation to be serialized
-     */
-    void setJsonObject( const QVariant& json );
-    
-    /* reimp */ void run();
+      /**
+       * Sets the json object to serialize.
+       *
+       * @param json QVariant containing the json representation to be serialized
+       */
+      void setJsonObject( const QVariant& json );
 
-  Q_SIGNALS:
-    /**
-    * This signal is emitted when the serialization process has been completed
-    * @param serialized contains the result of the serialization
-    * @param ok if a serialization error occurs ok is set to false, otherwise it's set to true.
-    * @param error_msg contains a string explaining the failure reason
-    **/
-    void parsingFinished(const QByteArray& serialized, bool ok, const QString& error_msg);
+      /* reimp */ void run();
 
-  private:
-    Q_DISABLE_COPY(SerializerRunnable)
-    class Private;
-    Private* const d;
-};
+    Q_SIGNALS:
+      /**
+      * This signal is emitted when the serialization process has been completed
+      * @param serialized contains the result of the serialization
+      * @param ok if a serialization error occurs ok is set to false, otherwise it's set to true.
+      * @param error_msg contains a string explaining the failure reason
+      **/
+      void parsingFinished(const QByteArray& serialized, bool ok, const QString& error_msg);
+
+    private:
+      Q_DISABLE_COPY(SerializerRunnable)
+      class Private;
+      Private* const d;
+  };
+}
 
 #endif // SERIALIZERRUNNABLE_H
