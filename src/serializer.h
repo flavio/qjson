d9c6018d77c89c3d9258f3f5a6c350da480736db
micron <micron@283d02a7-25f6-0310-bc7c-ecb5cbfe19da>
2009-11-30 19:19:51 +0000

    updated doxygen documentation
    git-svn-id: https://svn.kde.org/home/kde/trunk/playground/libs/qjson@1056750 283d02a7-25f6-0310-bc7c-ecb5cbfe19da

--- src/serializer.h
+++ src/serializer.h
@@ -37,6 +37,64 @@ namespace QJson {
   *
   * QVariant objects are converted to a string containing the JSON data.
   * If QVariant object is empty or not valid a <em>null</em> json object is returned.
+  *
+  * It's also possible to serialize a QObject instance  to JSON.
+  * All the class attributes defined as properties will be serialized.
+  *
+  * Suppose the declaration of Person class looks like this:
+  * \code
+  * class Person : public QObject
+    {
+      Q_OBJECT
+
+      Q_PROPERTY(QString name READ name WRITE setName)
+      Q_PROPERTY(int phoneNumber READ phoneNumber WRITE setPhoneNumber)
+      Q_PROPERTY(Gender gender READ gender WRITE setGender)
+      Q_PROPERTY(QDate dob READ dob WRITE setDob)
+      Q_ENUMS(Gender)
+
+     public:
+        Person(QObject* parent = 0);
+        ~Person();
+
+        QString name() const;
+        void setName(const QString& name);
+
+        int phoneNumber() const;
+        void setPhoneNumber(const int  phoneNumber);
+
+        enum Gender {Male, Female};
+        void setGender(Gender gender);
+        Gender gender() const;
+
+        QDate dob() const;
+        void setDob(const QDate& dob);
+
+      private:
+        QString m_name;
+        int m_phoneNumber;
+        Gender m_gender;
+        QDate m_dob;
+    };
+    \endcode
+
+    The following code will serialize an instance of Person to JSON :
+
+    \code
+    Person person;
+    person.setName("Flavio");
+    person.setPhoneNumber(123456);
+    person.setGender(Person::Male);
+    person.setDob(QDate(1982, 7, 12));
+
+    Serializer serializer;
+    qDebug() << serializer.serialize( &person);
+    \endcode
+
+    The generated output will be:
+    \code
+    { "dob" : "1982-07-12", "gender" : 0, "name" : "Flavio", "phoneNumber" : 123456 }
+    \endcode
   */
   class QJSON_EXPORT Serializer {
   public:
