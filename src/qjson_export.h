77c3b4c4e18a8a5f29fb15d416c428693cca34e9
Flavio Castelli <flavio@castelli.name>
2010-12-20 22:00:28 +0100

    Fix license section inside.
    QJson is distributed using the GNU Lesser General Public License but many files
    contained references to the GNU Library General Public License.

--- src/qjson_export.h
+++ src/qjson_export.h
@@ -2,7 +2,7 @@
     Copyright (C) 2009 Pino Toscano <pino@kde.org>
 
     This library is free software; you can redistribute it and/or
-    modify it under the terms of the GNU Library General Public
+    modify it under the terms of the GNU Lesser General Public
     License as published by the Free Software Foundation; either
     version 2 of the License, or (at your option) any later version.
 
@@ -11,7 +11,7 @@
     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
     Library General Public License for more details.
 
-    You should have received a copy of the GNU Library General Public License
+    You should have received a copy of the GNU Lesser General Public License
     along with this library; see the file COPYING.LIB.  If not, write to
     the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
     Boston, MA 02110-1301, USA.
