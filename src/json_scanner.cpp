flavio/qjson

commit 3f5cbad9d3021e5d61d3f2bd8b868fdf5f3f10f5

Fix build for Visual Studio 2008/2010. Did not test 2012, if it builds without these changes VC compiler version needs to be added to the ifdefs.

src/json_scanner.cpp
@@ -37,12 +37,20 @@ JSonScanner::JSonScanner(QIODevice* io)
     m_io (io),
     m_criticalError(false)
 {
+#ifdef Q_OS_WIN
+  m_C_locale = _create_locale(LC_NUMERIC, "C");
+#else
   m_C_locale = newlocale(LC_NUMERIC_MASK, "C", NULL);
+#endif
 }
 
 JSonScanner::~JSonScanner()
 {
-  freelocale(m_C_locale);
+#ifdef Q_OS_WIN
+  _free_locale(m_C_locale);
+#else
+  free_locale(m_C_locale);
+#endif
 }
 
 void JSonScanner::allowSpecialNumbers(bool allow) {

src/json_scanner.h
@@ -34,7 +34,12 @@
 #include "parser_p.h"
 
 #include <locale.h>
+
+#ifdef Q_OS_WIN
+#include <xlocale>
+#else
 #include <xlocale.h>
+#endif
 
 namespace yy {
   class location;
@@ -60,7 +65,11 @@ class JSonScanner : public yyFlexLexer
         yy::location* m_yylloc;
         bool m_criticalError;
         QString m_currentString;
+#ifdef Q_OS_WIN
+        _locale_t m_C_locale;
+#else
         locale_t m_C_locale;
+#endif
 };
 
 #endif

commit 4477f8c4935edc1be072ea70a87444a5a4211d69

Fixed unix build
Unix build got broken by commit 3f5cbad9d.

I also did a minor refactoring of the code shipped with commit 3f5cbad9d.

src/json_scanner.cpp
@@ -46,11 +46,7 @@ JSonScanner::JSonScanner(QIODevice* io)
 
 JSonScanner::~JSonScanner()
 {
-#ifdef Q_OS_WIN
-  _free_locale(m_C_locale);
-#else
-  free_locale(m_C_locale);
-#endif
+  freelocale(m_C_locale);
 }
 
 void JSonScanner::allowSpecialNumbers(bool allow) {

src/json_scanner.h
@@ -37,6 +37,8 @@
 
 #ifdef Q_OS_WIN
 #include <xlocale>
+#define locale_t _locale_t
+#define freelocale _free_locale
 #else
 #include <xlocale.h>
 #endif
@@ -65,11 +67,7 @@ class JSonScanner : public yyFlexLexer
         yy::location* m_yylloc;
         bool m_criticalError;
         QString m_currentString;
-#ifdef Q_OS_WIN
-        _locale_t m_C_locale;
-#else
         locale_t m_C_locale;
-#endif
 };
 
 #endif